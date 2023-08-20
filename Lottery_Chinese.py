a00bcc1e62857a4fab478775c2fccf5b63f6e062
Alexander-0001 <128902009+Alexander-0001@users.noreply.github.com>
2023-08-20 14:37:53 +0800

    Add files via upload

--- Lottery_Chinese.py
+++ Lottery_Chinese.py
@@ -1,20 +1,42 @@
-#用rkinter写一个1-100抽签
-from tkinter import *
+import tkinter as tk
 import random
 
-root = Tk()
-root.title("1-100抽签")
-root.geometry("300x100")
+root = tk.Tk()
+root.title('抽签')
+root.geometry('400x300')
 
-def get_number():
-    number = random.randint(1,100)
-    print(number)
-    label.config(text=number)
 
-label = Label(root,text="")
-label.pack()
 
-button = Button(root,text="抽签",command=get_number)
-button.pack()
+
+def get_value():
+    value = entry.get()
+    a = value.split('-')
+    if len(a) == 2:
+        sign = random.randint(int(a[0]),int(a[1]))
+        sign_label.config(text=sign)
+
+
+
+signal = tk.Label(root,text='请输入一个范围，格式: X-XX :',font=('黑体',20))
+signal.pack()
+
+sign = tk.Label(root,text='例: 1-100 1-50',font=('黑体',20))
+sign.pack()
+
+entry = tk.Entry(root,font=("黑体",30))
+entry.pack()
+
+sign = tk.Label(root,text="请输入要求格式",font=('黑体',20))
+sign.pack()
+
+get_sign_button = tk.Button(root,text='抽签',font=("黑体",30),command=get_value)
+get_sign_button.pack()
+
+sign_label = tk.Label(root,text='',font=('黑体',70))
+sign_label.pack()
+
+
+
+
 
 root.mainloop()
--- Lottery_English.py
+++ Lottery_English.py
@@ -1,20 +1,42 @@
-#用rkinter写一个1-100抽签
-from tkinter import *
+import tkinter as tk
 import random
 
-root = Tk()
-root.title("1-100 Draw")
-root.geometry("300x100")
+root = tk.Tk()
+root.title('DRAW')
+root.geometry('400x400')
 
-def get_number():
-    number = random.randint(1,100)
-    print(number)
-    label.config(text=number)
 
-label = Label(root,text="")
-label.pack()
 
-button = Button(root,text="Draw",command=get_number)
-button.pack()
+
+def get_value():
+    value = entry.get()
+    a = value.split('-')
+    if len(a) == 2:
+        b = random.randint(int(a[0]),int(a[1]))
+        sign_label.config(text=b)
+
+
+
+signal = tk.Label(root,text='Please enter a range,Form: X-XXX :',font=('黑体',16))
+signal.pack()
+
+sign = tk.Label(root,text='Example: 1-100 1-50',font=('黑体',19))
+sign.pack()
+
+entry = tk.Entry(root,font=("黑体",30))
+entry.pack()
+
+sign = tk.Label(root,text="PlEASE ENTER THE SPECTFIED FORM",font=('黑体',18))
+sign.pack()
+
+get_sign_button = tk.Button(root,text='DRAW',font=("黑体",30),command=get_value)
+get_sign_button.pack()
+
+sign_label = tk.Label(root,text='',font=('黑体',70))
+sign_label.pack()
+
+
+
+
 
 root.mainloop()
